414bc3744fe5ba73a9bf39315bf42ad5cf3a9a65
renav2 <kulkarnirenav255@gmail.com>
2021-10-26 19:39:18 +0530

    once again changes in log in page due to some error now 1)log in button work properly for the test purpose email:1@gmail.com pass:123456

--- app/src/main/java/com/example/abcs/MainActivity.java
+++ app/src/main/java/com/example/abcs/MainActivity.java
@@ -8,28 +8,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
-import com.firebase.ui.auth.IdpResponse;
-import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
-import com.firebase.ui.auth.util.ExtraConstants;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.ActionCodeSettings;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 
 
 public class MainActivity extends AppCompatActivity {
@@ -44,18 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    // [START auth_fui_create_launcher]
-    // See: https://developer.android.com/training/basics/intents/result
-    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
-            new FirebaseAuthUIActivityResultContract(),
-            new ActivityResultCallback<FirebaseAuthUIAuthenticationResult>() {
-                @Override
-                public void onActivityResult(FirebaseAuthUIAuthenticationResult result) {
-                    onSignInResult(result);
 
-                }
-            }
-    );
     // [END auth_fui_create_launcher]
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,30 +54,22 @@ homejump.setOnClickListener(new View.OnClickListener() {
     }
 });
 
-// END temp jump page for testing purpose
+bt_login.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        String tex_email=et_email.getText().toString();
+        String tex_pass=et_pass.getText().toString();
+        loginuser(tex_email,tex_pass);
+    }
+});
 
 
-        tv_jump2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                createSignInIntent();
-            }
-        });
 
-
-        bt_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String tex_email=et_email.getText().toString();
-                String tex_pass=et_pass.getText().toString();
-                loginuser(tex_email,tex_pass);
-            }
-        });
 
 
 
     }
-    //login button code
+
     private void loginuser(String _email, String _pass) {
         auth.signInWithEmailAndPassword(_email,_pass).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
             @Override
@@ -116,149 +80,7 @@ homejump.setOnClickListener(new View.OnClickListener() {
             }
         });
 
-
     }
-//log in button code ende here
-
-    public void createSignInIntent() {
-        // [START auth_fui_create_intent]
-        // Choose authentication providers
-        List<AuthUI.IdpConfig> providers = Arrays.asList(
-                new AuthUI.IdpConfig.EmailBuilder().build(),
-                new AuthUI.IdpConfig.PhoneBuilder().build(),
-                new AuthUI.IdpConfig.GoogleBuilder().build()
-              );
-
-        // Create and launch sign-in intent
-        Intent signInIntent = AuthUI.getInstance()
-                .createSignInIntentBuilder()
-                .setAvailableProviders(providers)
-                .build();
-        signInLauncher.launch(signInIntent);
-        // [END auth_fui_create_intent]
-    }
-
-    // [START auth_fui_result]
-    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
-        IdpResponse response = result.getIdpResponse();
-        if (result.getResultCode() == RESULT_OK) {
-            // Successfully signed in
-
-Intent intent=new Intent(this,RegisterActivity.class);
-startActivity(intent);
-
-            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-            Toast.makeText(this, "succesfully sign in", Toast.LENGTH_SHORT).show();
-            // ...
-        } else {
-            // Sign in failed. If response is null the user canceled the
-            // sign-in flow using the back button. Otherwise check
-            // response.getError().getErrorCode() and handle the error.
-            // ...
-        }
-    }
-    // [END auth_fui_result]
-
-    public void signOut() {
-        // [START auth_fui_signout]
-        AuthUI.getInstance()
-                .signOut(this)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    public void onComplete(@NonNull Task<Void> task) {
-                        // ...
-                    }
-                });
-        // [END auth_fui_signout]
-    }
-
-    public void delete() {
-        // [START auth_fui_delete]
-        AuthUI.getInstance()
-                .delete(this)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        // ...
-                    }
-                });
-        // [END auth_fui_delete]
-    }
-
-    public void themeAndLogo() {
-        List<AuthUI.IdpConfig> providers = Collections.emptyList();
-
-        // [START auth_fui_theme_logo]
-        Intent signInIntent = AuthUI.getInstance()
-                .createSignInIntentBuilder()
-                .setAvailableProviders(providers)
-//                .setLogo(R.drawable.my_great_logo)      // Set logo drawable
-//                .setTheme(R.style.MySuperAppTheme)      // Set theme
-                .build();
-        signInLauncher.launch(signInIntent);
-        // [END auth_fui_theme_logo]
-    }
-
-    public void privacyAndTerms() {
-        List<AuthUI.IdpConfig> providers = Collections.emptyList();
-
-        // [START auth_fui_pp_tos]
-        Intent signInIntent = AuthUI.getInstance()
-                .createSignInIntentBuilder()
-                .setAvailableProviders(providers)
-                .setTosAndPrivacyPolicyUrls(
-                        "https://example.com/terms.html",
-                        "https://example.com/privacy.html")
-                .build();
-        signInLauncher.launch(signInIntent);
-        // [END auth_fui_pp_tos]
-    }
-
-    public void emailLink() {
-        // [START auth_fui_email_link]
-        ActionCodeSettings actionCodeSettings = ActionCodeSettings.newBuilder()
-                .setAndroidPackageName(
-                        /* yourPackageName= */ "...",
-                        /* installIfNotAvailable= */ true,
-                        /* minimumVersion= */ null)
-                .setHandleCodeInApp(true) // This must be set to true
-                .setUrl("https://google.com") // This URL needs to be whitelisted
-                .build();
-
-        List<AuthUI.IdpConfig> providers = Arrays.asList(
-                new AuthUI.IdpConfig.EmailBuilder()
-                        .enableEmailLinkSignIn()
-                        .setActionCodeSettings(actionCodeSettings)
-                        .build()
-        );
-        Intent signInIntent = AuthUI.getInstance()
-                .createSignInIntentBuilder()
-                .setAvailableProviders(providers)
-                .build();
-        signInLauncher.launch(signInIntent);
-        // [END auth_fui_email_link]
-    }
-
-    public void catchEmailLink() {
-        List<AuthUI.IdpConfig> providers = Collections.emptyList();
-
-        // [START auth_fui_email_link_catch]
-        if (AuthUI.canHandleIntent(getIntent())) {
-            if (getIntent().getExtras() == null) {
-                return;
-            }
-            String link = getIntent().getExtras().getString(ExtraConstants.EMAIL_LINK_SIGN_IN);
-            if (link != null) {
-                Intent signInIntent = AuthUI.getInstance()
-                        .createSignInIntentBuilder()
-                        .setEmailLink(link)
-                        .setAvailableProviders(providers)
-                        .build();
-                signInLauncher.launch(signInIntent);
-            }
-        }
-        // [END auth_fui_email_link_catch]
-    }
-    // direct log in
 
 
 }
---
+++ app/src/main/java/com/example/abcs/social_media_login_Activity.java
@@ -0,0 +1,14 @@
+package com.example.abcs;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class social_media_login_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_social_media_login);
+    }
+}
