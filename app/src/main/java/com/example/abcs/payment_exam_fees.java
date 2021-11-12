5837922ffccee791e07ec45cba7c62c5a19f82f3
renav2 <kulkarnirenav255@gmail.com>
2021-11-12 14:50:13 +0530

    last section also done now working on invoice genration

--- app/src/main/java/com/example/abcs/payment_exam_fees.java
+++ app/src/main/java/com/example/abcs/payment_exam_fees.java
@@ -1,12 +1,21 @@
 package com.example.abcs;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class payment_exam_fees extends AppCompatActivity {
+import com.razorpay.Checkout;
+import com.razorpay.PaymentData;
+import com.razorpay.PaymentResultWithDataListener;
+
+import org.json.JSONObject;
+
+public class payment_exam_fees extends AppCompatActivity  implements PaymentResultWithDataListener {
 private Button btn_exampay;
 private EditText txt_examamount;
 
@@ -18,6 +27,72 @@ private EditText txt_examamount;
         btn_exampay=findViewById(R.id.btn_examfees);
         txt_examamount=findViewById(R.id.txt_examamount);
 
+        btn_exampay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String samount=txt_examamount.getText().toString();
+                int amount = Math.round(Float.parseFloat(samount) * 100);
+                makepay(amount);
+            }
+        });
+
+    }
+
+    private void makepay(int amount) {
+        Checkout checkout = new Checkout();
+
+        checkout.setKeyID("rzp_test_9nCAqrhuTfsGcZ");
+
+        /**
+         * Instantiate Checkout
+         */
+
+
+        /**
+         * Set your logo here
+         */
+
+
+        /**
+         * Reference to current activity
+         */
+        final Activity activity = this;
+
+        /**
+         * Pass your payment options to the Razorpay Checkout as a JSONObject
+         */
+        try {
+            JSONObject options = new JSONObject();
+
+            options.put("name", "sinhagad");
+            options.put("description", "Reference No. #123456");
+            options.put("image", "https://s3.amazonaws.com/rzp-mobile/images/rzp.png");
+            // options.put("order_id", "order_DBJOWzybf0sJbb");//from response of step 3.
+            options.put("theme.color", "#3399cc");
+            options.put("currency", "INR");
+            options.put("amount",amount);//pass amount in currency subunits
+            options.put("prefill.email"," ");
+            options.put("prefill.contact"," ");
+            JSONObject retryObj = new JSONObject();
+            retryObj.put("enabled", true);
+            retryObj.put("max_count", 4);
+            options.put("retry", retryObj);
+
+            checkout.open(activity, options);
+
+
+        } catch(Exception e) {
+            Log.e("TAG", "Error in starting Razorpay Checkout", e);
+        }
+    }
+
+    @Override
+    public void onPaymentSuccess(String s, PaymentData paymentData) {
+
+    }
+
+    @Override
+    public void onPaymentError(int i, String s, PaymentData paymentData) {
 
     }
 }
