bdbbe303ae38771c7f0cb6f420c95501cf95dd2a
renav2 <kulkarnirenav255@gmail.com>
2021-12-30 23:03:53 +0530

    new invoice page added but not connected

---
+++ app/src/main/java/com/example/abcs/payment_invoi.java
@@ -0,0 +1,213 @@
+package com.example.abcs;
+
+import android.Manifest;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.graphics.pdf.PdfDocument;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.Display;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.NestedScrollView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Random;
+
+public class payment_invoi extends AppCompatActivity {
+    Display mDisplay;
+    String imagesUri;
+    String path;
+    Bitmap bitmap;
+
+    int totalHeight;
+    int totalWidth;
+
+    public static final int READ_PHONE = 110;
+    String file_name = "collage payment section";
+    File myPath;
+    TextView tv_invoiceno;
+    Button btn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_invoi);
+        tv_invoiceno=findViewById(R.id.tv_invoiceno);
+
+
+        btn=findViewById(R.id.btn_print);
+
+
+        WindowManager wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+        mDisplay = wm.getDefaultDisplay();
+
+
+        if(Build.VERSION.SDK_INT >= 23){
+            if(checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+                    && checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED){
+            }else{
+                requestPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE}, READ_PHONE);
+            }
+        }
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                invoice_no();
+                Print_Button_invisible();
+
+                takeScreenShot();
+
+            }
+        });
+
+    }
+
+    private void Print_Button_invisible() {
+        btn.setVisibility(View.INVISIBLE);
+
+    }
+
+    private void invoice_no() {
+        Random random=new Random();
+        int val=random.nextInt(1000000000);
+        int val2=random.nextInt(1000000000);
+        if(val ==val2){
+            tv_invoiceno.setText("E0"+val);
+        }
+
+        tv_invoiceno.setText(Integer.toString(val));
+
+    }
+
+    private void takeScreenShot() {
+        File folder = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/ScreenShot/");
+
+        if(!folder.exists()){
+            boolean success = folder.mkdir();
+        }
+
+        path = folder.getAbsolutePath();
+        path = path + "/" + file_name + System.currentTimeMillis() + ".pdf";
+
+
+        View u = findViewById(R.id.ticket);
+
+        NestedScrollView z = findViewById(R.id.ticket);
+        totalHeight = z.getChildAt(0).getHeight();
+        totalWidth = z.getChildAt(0).getWidth();
+
+
+
+
+        String extr = Environment.getExternalStorageDirectory() + "/Flight Ticket/";
+        File file = new File(extr);
+        if(!file.exists())
+            file.mkdir();
+        String fileName = file_name + ".jpg";
+        myPath = new File(extr, fileName);
+        imagesUri = myPath.getPath();
+        bitmap = getBitmapFromView(u, totalHeight, totalWidth);
+
+        try{
+            FileOutputStream fos = new FileOutputStream(myPath);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
+            fos.flush();
+            fos.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        createPdf();
+
+    }
+
+    private Bitmap getBitmapFromView(View view, int totalHeight, int totalWidth) {
+        Bitmap returnedBitmap = Bitmap.createBitmap(totalWidth, totalHeight, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(returnedBitmap);
+        Drawable bgDrawable = view.getBackground();
+
+        if(bgDrawable != null){
+            bgDrawable.draw(canvas);
+        }else{
+            canvas.drawColor(Color.WHITE);
+        }
+
+        view.draw(canvas);
+        return returnedBitmap;
+
+
+
+    }
+
+    private void createPdf() {
+        PdfDocument document = new PdfDocument();
+        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(bitmap.getWidth(), bitmap.getHeight(), 1).create();
+        PdfDocument.Page page = document.startPage(pageInfo);
+
+        Canvas canvas = page.getCanvas();
+
+        Paint paint = new Paint();
+        paint.setColor(Color.parseColor("#ffffff"));
+        canvas.drawPaint(paint);
+
+        Bitmap bitmap = Bitmap.createScaledBitmap(this.bitmap, this.bitmap.getWidth(), this.bitmap.getHeight(), true);
+
+        paint.setColor(Color.BLUE);
+        canvas.drawBitmap(bitmap, 0, 0, null);
+        document.finishPage(page);
+        File filePath = new File(path);
+        try{
+            document.writeTo(new FileOutputStream(filePath));
+        }catch (IOException e){
+            e.printStackTrace();
+            Toast.makeText(this, "Something Wrong: "+e.toString(), Toast.LENGTH_SHORT).show();
+        }
+
+        document.close();
+
+        if (myPath.exists())
+            myPath.delete();
+
+        openPdf(path);
+    }
+
+    private void openPdf(String path) {
+        File file = new File(path);
+        Intent target = new Intent(Intent.ACTION_VIEW);
+        target.setDataAndType(Uri.fromFile(file), "application/pdf");
+        target.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+
+        Intent intent = Intent.createChooser(target, "Open FIle");
+        try{
+
+
+
+            startActivity(intent);
+        }catch (ActivityNotFoundException e){
+            Toast.makeText(this, "No Apps to read PDF FIle", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
--- app/src/main/java/com/example/abcs/paymentclg.java
+++ app/src/main/java/com/example/abcs/paymentclg.java
@@ -1,6 +1,7 @@
 package com.example.abcs;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -103,6 +104,8 @@ String inp;
 
     @Override
     public void onPaymentSuccess(String s, PaymentData paymentData) {
+        Intent intent =new Intent(paymentclg.this,payment_invoi.class);
+        startActivity(intent);
 
     }
 
