8c94537586636cb0208612c16b583d670fbe68d2
renav2 <kulkarnirenav255@gmail.com>
2022-01-15 23:42:13 +0530

    hostel payment backed remain

--- app/src/main/java/com/example/abcs/Payment_hostel_fees_invoice.java
+++ app/src/main/java/com/example/abcs/Payment_hostel_fees_invoice.java
@@ -10,5 +10,7 @@ public class Payment_hostel_fees_invoice extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_hostel_fees_invoice);
+
+
     }
 }
