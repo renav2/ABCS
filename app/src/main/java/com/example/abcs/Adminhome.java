ddba49ec31ad5b3b013b02a85b109d81c3b32978
Sharayu017 <pasalkars725@gmail.com>
2022-01-18 14:39:01 +0530

    mainfest file error resolved

--- app/src/main/java/com/example/abcs/Adminhome.java
+++ app/src/main/java/com/example/abcs/Adminhome.java
@@ -1,6 +1,8 @@
 package com.example.abcs;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,17 +13,17 @@ Button apermission;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adminhome);
-       // apermission=(Button) findViewById(R.id.adminper);
-//        apermission.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                openPermissiionActivity();
-//            }
-//
-//            private void openPermissiionActivity() {
-//                Intent intent = new Intent(Adminhome.this, AdminPermissionHome.class);
-//                startActivity(intent);
-//            }
-//        });
+        apermission = (Button) findViewById(R.id.adminper);
+        apermission.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openPermissiionActivity();
+            }
+        });
+    }
+
+    private void openPermissiionActivity() {
+        Intent intent = new Intent(this, AdminPermissionHome.class);
+        startActivity(intent);
     }
 }
