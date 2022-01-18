644ae2629c921d2d2c5a87256d181bea514ca931
renav2 <kulkarnirenav255@gmail.com>
2022-01-18 14:22:34 +0530

    admin basic page add

--- app/src/main/java/com/example/abcs/Adminhome.java
+++ app/src/main/java/com/example/abcs/Adminhome.java
@@ -1,8 +1,6 @@
 package com.example.abcs;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,16 +12,16 @@ Button apermission;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adminhome);
        // apermission=(Button) findViewById(R.id.adminper);
-        apermission.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openPermissiionActivity();
-            }
-
-            private void openPermissiionActivity() {
-                Intent intent = new Intent(Adminhome.this, AdminPermissionHome.class);
-                startActivity(intent);
-            }
-        });
+//        apermission.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                openPermissiionActivity();
+//            }
+//
+//            private void openPermissiionActivity() {
+//                Intent intent = new Intent(Adminhome.this, AdminPermissionHome.class);
+//                startActivity(intent);
+//            }
+//        });
     }
 }
