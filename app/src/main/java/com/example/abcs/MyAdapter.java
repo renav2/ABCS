77c511725f3586bd167cde09d20a7b006aa3bdfa
Shubham Habbu <shubham.habbu90@gmail.com>
2022-01-20 17:15:03 +0530

    Pdf fetch

---
+++ app/src/main/java/com/example/abcs/MyAdapter.java
@@ -0,0 +1,75 @@
+package com.example.abcs;
+
+import android.annotation.SuppressLint;
+import android.app.DownloadManager;
+import android.content.Context;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import static android.os.Environment.DIRECTORY_DOWNLOADS;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
+
+    Test_main test_main;
+    ArrayList<DownModel> downModels;
+    private MyViewHolder myViewHolder;
+    private int i;
+//    int i;
+
+    public MyAdapter( Test_main test_main, ArrayList<DownModel> downModels) {
+        this.test_main = test_main;
+        this.downModels = downModels;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+
+        LayoutInflater layoutInflater = LayoutInflater.from(test_main.getBaseContext());
+        View view = layoutInflater.inflate(R.layout.test_element, null, false);
+
+        return new MyViewHolder(view);
+    }
+
+    @SuppressLint("RecyclerView")
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
+
+
+        myViewHolder.mName.setText(downModels.get(i).getName());
+        myViewHolder.mLink.setText(downModels.get(i).getLink());
+        myViewHolder.mDownload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                downloadFile(myViewHolder.mName.getContext(),downModels.get(i).getName(),".pdf",DIRECTORY_DOWNLOADS,downModels.get(i).getLink());
+            }
+        });
+
+
+    }
+
+    public void downloadFile(Context context, String fileName, String fileExtension, String destinationDirectory, String url) {
+
+        DownloadManager downloadmanager = (DownloadManager) context.
+                getSystemService(Context.DOWNLOAD_SERVICE);
+        Uri uri = Uri.parse(url);
+        DownloadManager.Request request = new DownloadManager.Request(uri);
+
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        request.setDestinationInExternalFilesDir(context, destinationDirectory, fileName + fileExtension);
+
+        downloadmanager.enqueue(request);
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return downModels.size();
+    }
+}
