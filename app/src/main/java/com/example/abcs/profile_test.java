aa17b02986de55e946e01a63c36b6d683852c12d
Sharayu017 <pasalkars725@gmail.com>
2022-01-31 16:00:00 +0530

    manifest change

--- app/src/main/java/com/example/abcs/profile_test.java
+++ app/src/main/java/com/example/abcs/profile_test.java
@@ -28,7 +28,7 @@ import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
-import com.squareup.picasso.Picasso;
+//import com.squareup.picasso.Picasso;
 
 public class profile_test extends AppCompatActivity {
 
@@ -68,7 +68,7 @@ public class profile_test extends AppCompatActivity {
             @Override
             public void onSuccess(Uri uri) {
 
-                Picasso.get().load(uri).into(pimage);
+                //Picasso.get().load(uri).into(pimage);
             }
         });
         userID=fAuth.getCurrentUser().getUid();
@@ -123,7 +123,7 @@ public class profile_test extends AppCompatActivity {
                 fileref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
                     @Override
                     public void onSuccess(Uri uri) {
-                        Picasso.get().load(uri).into(pimage);
+                        //Picasso.get().load(uri).into(pimage);
                         Toast.makeText(profile_test.this, "profile uplode", Toast.LENGTH_SHORT).show();
                     }
                 });
