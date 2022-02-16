7c2990513ce007f050e412e1c7e0ffce41f36fef
renav2 <kulkarnirenav255@gmail.com>
2022-02-16 10:45:19 +0530

    payment ticket add

---
+++ app/src/main/java/com/example/abcs/Admin_Support_paymentsection.java
@@ -0,0 +1,14 @@
+package com.example.abcs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Admin_Support_paymentsection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_support_paymentsection);
+    }
+}
