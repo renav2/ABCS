99c263a2cc7ea0b64f855d5eb2ce230fd4f8ae18
renav2 <kulkarnirenav255@gmail.com>
2022-02-22 13:24:50 +0530

    support pay universal paystorage added

--- app/src/main/java/com/example/abcs/Support_pay.java
+++ app/src/main/java/com/example/abcs/Support_pay.java
@@ -1,5 +1,6 @@
 package com.example.abcs;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
@@ -20,6 +21,7 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
@@ -39,15 +41,22 @@ import java.util.List;
 import java.util.Map;
 
 public class Support_pay extends AppCompatActivity implements PaymentResultWithDataListener {
-    String samount;
+
     EditText et1,enteramount;
     Button sub,paybutton;
     FirebaseFirestore fstore;
     FirebaseAuth auth;
     String name,email,unicid,branch,_class,aaaaa;
     Spinner installments;
+TextView m,n,b,v,c;
 
-TextView vvasdaas,chutya,gul;
+   //installemets
+    String samount;
+    String thramo;
+    String four;
+    TextView f,s,t,fo;
+    TextView a1,a2,a3,a4;
+    TextView vvasdaas,chutya,gul;
     String pp;
 
     @Override
@@ -56,6 +65,30 @@ TextView vvasdaas,chutya,gul;
         setContentView(R.layout.activity_support_pay);
 
 
+
+                m=findViewById(R.id.m);
+                n=findViewById(R.id.n);
+                b=findViewById(R.id.b);
+                v=findViewById(R.id.v);
+                c=findViewById(R.id.c);
+
+                f=findViewById(R.id.insta1);
+                s=findViewById(R.id.insta2);
+                t=findViewById(R.id.insta3);
+                fo=findViewById(R.id.insta4);
+                a1=findViewById(R.id.amo1);
+                a2=findViewById(R.id.amo2);
+                a3=findViewById(R.id.amo3);
+                a4=findViewById(R.id.amo4);
+
+
+
+
+        m.setVisibility(View.INVISIBLE);
+        n.setVisibility(View.INVISIBLE);
+        b.setVisibility(View.INVISIBLE);
+        v.setVisibility(View.INVISIBLE);
+        c.setVisibility(View.INVISIBLE);
 
             et1=findViewById(R.id.editTextTextMultiLine5);
             enteramount=findViewById(R.id.editTextTextPersonName);
@@ -66,18 +99,81 @@ TextView vvasdaas,chutya,gul;
             installments=findViewById(R.id.pay_in2);
             vvasdaas=findViewById(R.id.l12);
             chutya=findViewById(R.id.textView77);
-gul=findViewById(R.id.textView133);
-
-
+            gul=findViewById(R.id.textView133);
             //support
             installments.setVisibility(View.INVISIBLE);
             enteramount.setVisibility(View.INVISIBLE);
             paybutton.setVisibility(View.INVISIBLE);
             chutya.setVisibility(View.INVISIBLE);
-vvasdaas.setVisibility(View.INVISIBLE);
+            vvasdaas.setVisibility(View.INVISIBLE);
 //BASIC HANDLER
 
         try{
+
+
+
+
+
+
+//data for  1st  installment
+            fstore.collection("Final_paymnet_data").whereEqualTo("Email",auth.getCurrentUser().getEmail()).whereEqualTo("installment","First").get()
+                    .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                        @Override
+                        public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                            List<DocumentSnapshot> list=queryDocumentSnapshots.getDocuments();
+                            for(DocumentSnapshot d:list)
+                            {
+                                f.setText(d.getString("installment"));
+                                a1.setText(d.getString("paidamount"));
+                            }
+
+                        }
+                    });
+//dat 2nd
+//
+//            fstore.collection("financial_problem_student_paymentdata").whereEqualTo("Email",auth.getCurrentUser().getEmail()).get()
+//                    .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+//                        @Override
+//                        public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+//                            List<DocumentSnapshot> list=queryDocumentSnapshots.getDocuments();
+//                            for(DocumentSnapshot d:list)
+//                            {
+//                                f.setText(d.getString("installment"));
+//                                a1.setText(d.getString("paidamount"));
+//                            }
+//
+//                        }
+//                    });
+
+
+
+
+
+//            DocumentReference documentReference=fstore.collection("demo").document(auth.getCurrentUser().getUid());
+//            documentReference.addSnapshotListener(Support_pay.this, new EventListener<DocumentSnapshot>() {
+//                @Override
+//                public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+//                    String s,s1;
+//                    name=value.getString("Name");
+//                    email=value.getString("Email");
+//                    // unicid=value.getString("assignno");
+//                    branch =value.getString("Branch");
+//                    _class=value.getString("_Class");
+//                    vvasdaas.setText(value.getString("assignno"));
+//                    pp=vvasdaas.getText().toString();
+//                    //  vv.setText(unicid);
+//
+//                }
+//
+//            });
+
+
+
+
+
+            ///data from demo
+
+
 
             DocumentReference documentReference=fstore.collection("demo").document(auth.getCurrentUser().getUid());
             documentReference.addSnapshotListener(Support_pay.this, new EventListener<DocumentSnapshot>() {
@@ -92,8 +188,44 @@ vvasdaas.setVisibility(View.INVISIBLE);
                     vvasdaas.setText(value.getString("assignno"));
                     pp=vvasdaas.getText().toString();
                     //  vv.setText(unicid);
+
+                    m.setText(name);
+                    n.setText(email);
+                    b.setText(branch);
+                    v.setText(_class);
+                    c.setText(pp);
+up(m,n,b,v,c);
+
+
                     fatke(pp);
                 }
+
+                private void up(TextView m, TextView n, TextView b, TextView v, TextView c) {
+
+                    DocumentReference d3=fstore.collection("financial_problem_student_paymentdata").document(auth.getCurrentUser().getUid());
+                    Map<String, String> vv=new HashMap<>();
+
+                    vv.put("Name",m.getText().toString());
+                    vv.put("assignno",c.getText().toString());
+                    vv.put("Email",n.getText().toString());
+                    vv.put("Branch",b.getText().toString());
+                    vv.put("Class",v.getText().toString());
+             ///       vv.put("installemt",installments.getSelectedItem().toString());
+                    vv.put("amountpaidsecond","0");
+                    vv.put("amountpaythird","0");
+                    vv.put("amountfour","0");
+
+
+                    d3.set(vv).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void unused) {
+                            Toast.makeText(Support_pay.this, "Your request is send  ", Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
+
+                }
+
                 private void fatke(String pp) {
                     Toast.makeText(Support_pay.this, pp, Toast.LENGTH_SHORT).show();
                     DocumentReference documentReference1= fstore.collection("Support_payment_issue").document(pp);
@@ -120,6 +252,12 @@ vvasdaas.setVisibility(View.INVISIBLE);
                 }
             });
 
+
+
+
+
+
+
         }catch (Exception e){ Toast.makeText(Support_pay.this, "something went wrong", Toast.LENGTH_SHORT).show(); }
 
 
@@ -133,13 +271,12 @@ vvasdaas.setVisibility(View.INVISIBLE);
 
 
 
+
+
+
                         samount=enteramount.getText().toString();
                         int amount = Math.round(Float.parseFloat(samount) * 100);
                         makepay(amount);
-
-
-
-
                 }
             });
 
@@ -233,25 +370,83 @@ vvasdaas.setVisibility(View.INVISIBLE);
     public void onPaymentSuccess(String s, PaymentData paymentData) {
         Intent intent =new Intent(Support_pay.this, Payment_Collage_invoice.class);
         int amount = Math.round(Float.parseFloat(samount) * 100);
-        Toast.makeText(this, "paymentsuccesfull", Toast.LENGTH_SHORT).show();
-        DocumentReference d3=fstore.collection("financial_problem_student_paymentdata").document();
-        Map<String, String> vv=new HashMap<>();
-        vv.put("Name",name);
-        vv.put("assignno",unicid);
-        vv.put("Email",email);
-        vv.put("Branch",branch);
-        vv.put("Class",_class);
-        vv.put("installemt",installments.getSelectedItem().toString());
-        vv.put("amountpaid",samount);
-        d3.set(vv).addOnSuccessListener(new OnSuccessListener<Void>() {
-            @Override
-            public void onSuccess(Void unused) {
-                Toast.makeText(Support_pay.this, "Your request is send  ", Toast.LENGTH_SHORT).show();
-                Intent intent =new Intent(Support_pay.this,MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+
+
+
+
+        String sec=samount;
+        String thir=samount;
+        String four=samount;
+
+
+
+
+        if(installments.getSelectedItem().toString().equals("Second")){
+            DocumentReference washingtonRef = fstore.collection("financial_problem_student_paymentdata").document(auth.getCurrentUser().getUid());
+
+
+            washingtonRef
+                    .update("amountpaidsecond", samount)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Toast.makeText(Support_pay.this, "Second installment payment  successful ", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                        }
+                    });
+        }
+
+
+         if(installments.getSelectedItem().toString().equals("Third")){
+            DocumentReference washingtonRef = fstore.collection("financial_problem_student_paymentdata").document(auth.getCurrentUser().getUid());
+
+            washingtonRef
+                    .update("amountpaythird", thir)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Toast.makeText(Support_pay.this, "Third installment payment  successful ", Toast.LENGTH_SHORT).show();
+
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                        }
+                    });
+        }
+
+         if(installments.getSelectedItem().toString().equals("Fourth")){
+            DocumentReference washingtonRef = fstore.collection("financial_problem_student_paymentdata").document(auth.getCurrentUser().getUid());
+            washingtonRef
+                    .update("amountfour", four)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Toast.makeText(Support_pay.this, "Fourth installment payment  successful ", Toast.LENGTH_SHORT).show();
+
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                        }
+                    });
+
+        }
+
+
+
+
+
+
+
+
     }
     @Override
     public void onPaymentError(int i, String s, PaymentData paymentData) {
