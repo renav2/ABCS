915a1583f9834c9fa30ae73d92a1346e37fa242b
Shubham Habbu <shubham.habbu90@gmail.com>
2022-02-27 14:57:53 +0530

    Account side UI changes

--- app/src/main/java/com/example/abcs/Account_Home.java
+++ app/src/main/java/com/example/abcs/Account_Home.java
@@ -3,15 +3,30 @@ package com.example.abcs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
 
 public class Account_Home extends AppCompatActivity {
 
     Button profile,payment,document,del,up,FEE;
     TextView mail;
+    ImageView img;
+    StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +42,25 @@ public class Account_Home extends AppCompatActivity {
         payment=findViewById(R.id.pay);
         document=findViewById(R.id.doc);
 
+        img=findViewById(R.id.acimage);
+
         mail.setText(getIntent().getStringExtra("Teacher_email"));
+
+
+        //profile pic
+        storageReference= FirebaseStorage.getInstance().getReference();
+        //currunt user id store in r  & for sring purpose
+        FirebaseUser user1 = FirebaseAuth.getInstance().getCurrentUser();
+        StorageReference profileRef= storageReference.child("Account/"+mail.getText().toString()+"/"+"/Profile.jpg");
+        profileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.get().load(uri).into(img);
+            }
+        });
+
+
+
 
         FEE.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -81,5 +114,32 @@ public class Account_Home extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.logO:
+                signout();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void signout() {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+        Toast.makeText(Account_Home.this, "Logout Successfully", Toast.LENGTH_SHORT).show();
+        finish();
     }
 }
